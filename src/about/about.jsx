a6f242e10519c019377f1383d1698fe90a8c97f7
Hernã Saldanha <hernags@mpf.mp.br>
2018-05-22 15:32:03 -0300

    Inicial

---
+++ src/about/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader';
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós' />
+        <h2>Nossa História</h2>
+        <p>Texto sobre a nossa história</p>
+        <h2>Missão</h2>
+        <p>Texto sobre a Missão</p>
+    </div>
+)
